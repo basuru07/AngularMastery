320a5b469a9d58a367b34a5a56b701e1c1951ccb
kasun_l <yasaruwanbasuru637@gmail.com>
2025-08-27 16:28:23 +0530

    Update the validation parts

--- src/app/user-setup/user-setup.component.ts
+++ src/app/user-setup/user-setup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-setup',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSetupComponent implements OnInit {
 
-  constructor() { }
+  userForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      gender: ['', Validators.required],
+      dob: [null, Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', Validators.required],
+      customerId: ['', Validators.required],
+      membership: ['', Validators.required]
+    });
   }
 
+  onSubmit(): void {
+    if (this.userForm.valid) {
+      console.log('Form Submitted:', this.userForm.value);
+      this.userForm.reset();
+    }
+    else{
+      Object.values(this.userForm.controls).forEach(control =>{
+        if(control.invalid){
+          control.markAsDirty();
+          control.updateValueAndValidity({onlySelf:true});
+        }
+      })
+    }
+  }
 }
