ec342073048359795f5ec35285021d4842c83811
kasun_l <yasaruwanbasuru637@gmail.com>
2025-08-28 14:04:37 +0530

    dev: creating the 3 pages

---
+++ src/app/bodytext/bodytext.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bodytext',
+  templateUrl: './bodytext.component.html',
+  styleUrls: ['./bodytext.component.css']
+})
+export class BodytextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
